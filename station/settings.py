6edc602c7d04c5468c25515b7adcc7c49a48b909
刘阳 <249606097@qq.com>
2021-04-27 20:21:49 +0800

    feat: 配置 MySQL 数据库

--- station/settings.py
+++ station/settings.py
@@ -84,8 +84,14 @@ WSGI_APPLICATION = 'station.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': config.get('MYSQL_DATABASE_NAME'),
+        'USER': config.get('MYSQL_DATABASE_USER'),
+        'PASSWORD': config.get('MYSQL_DATABASE_PASSWORD'),
+
+        'HOST': config.get('MYSQL_DATABASE_HOST'),
+        'PORT': config.get('MYSQL_DATABASE_PORT')
     }
 }
 
