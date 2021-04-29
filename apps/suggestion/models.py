94e72ff0967d55f3facac9d37cabd4faba3913c5
刘阳 <249606097@qq.com>
2021-04-29 19:59:02 +0800

    feat: 意见箱功能

--- apps/suggestion/models.py
+++ apps/suggestion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Suggestion(models.Model):
+    account = models.ForeignKey('Account', verbose_name='用户', on_delete=models.CASCADE)
+    content = models.TextField(verbose_name='内容', null=False, blank=False)
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+
+    reply = models.TextField(verbose_name='回复', null=False, blank=False)
+
+    class Meta:
+        verbose_name = '意见'
+        verbose_name_plural = verbose_name
--- apps/suggestion/views.py
+++ apps/suggestion/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from apps.utils.response_processor import process_response
+from apps.utils.response_status import ResponseStatus
+from apps.utils.decorator import Protect, RequiredMethod, LoginRequired
+from apps.account import models as account_models
+from apps.suggestion import models as suggestion_models
+
+
+@RequiredMethod('POST')
+@LoginRequired
+def post_suggestion(request):
+    request_data = json.loads(request.body)
+
+    content = request_data.get('content')
+    if not content:
+        return process_response(request, ResponseStatus.MISSING_PARAMETER_ERROR)
+
+    account = account_models.Account.objects.filter(openid=request.session['openid']).first()
+
+    suggestion = suggestion_models.Suggestion(account=account, content=content)
+    suggestion.save()
+
+    return process_response(request, ResponseStatus.OK)
+
+
+@RequiredMethod('GET')
+@LoginRequired
+def get_my_suggestions(request):
+    request.data = {
+        'suggestions': []
+    }
+
+    account = account_models.Account.objects.filter(openid=request.session['openid']).first()
+    suggestions = suggestion_models.Suggestion.objects.filter(account=account).order_by('-create_time')
+
+    for one in suggestions:
+        request.data['suggestions'].append({
+            'content': one.content,
+            'create_time': one.create_time,
+            'reply': one.reply
+        })
+
+    return process_response(request, ResponseStatus.OK)
