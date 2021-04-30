29fc82ef6f61b325243eea187b27baf3559ebe52
刘阳 <249606097@qq.com>
2021-04-30 13:39:26 +0800

    fix: 修复外键引用错误

--- apps/bbs/models.py
+++ apps/bbs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    account = models.ForeignKey('Account', verbose_name='用户', on_delete=models.CASCADE)
+    account = models.ForeignKey('account.Account', verbose_name='用户', on_delete=models.CASCADE)
     anonymous = models.BooleanField(verbose_name='是否匿名', default=False)
 
     title = models.CharField(verbose_name='标题', max_length=100, null=False, blank=False)
@@ -15,10 +15,10 @@ class Topic(models.Model):
 
 
 class Comment(models.Model):
-    account = models.ForeignKey('Account', verbose_name='用户', on_delete=models.CASCADE)
+    account = models.ForeignKey('account.Account', verbose_name='用户', on_delete=models.CASCADE)
     anonymous = models.BooleanField(verbose_name='是否匿名', default=False)
-
-    topic = models.ForeignKey('Topic', verbose_name='主题', on_delete=models.CASCADE)
+ 
+    topic = models.ForeignKey('bbs.Topic', verbose_name='主题', on_delete=models.CASCADE)
     content = models.TextField(verbose_name='内容', null=False, blank=False)
     create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
 
--- apps/suggestion/models.py
+++ apps/suggestion/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Suggestion(models.Model):
-    account = models.ForeignKey('Account', verbose_name='用户', on_delete=models.CASCADE)
+    account = models.ForeignKey('account.Account', verbose_name='用户', on_delete=models.CASCADE)
     content = models.TextField(verbose_name='内容', null=False, blank=False)
     create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
 
