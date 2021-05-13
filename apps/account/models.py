d1213f15c40e749b4502bb564b3b664b19224500
刘阳 <249606097@qq.com>
2021-05-13 14:09:10 +0800

    feat: unionid 可为 NULL

--- apps/account/models.py
+++ apps/account/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Account(models.Model):
     openid = models.CharField(verbose_name='openid', max_length=255, null=False, blank=False)
-    unionid = models.CharField(verbose_name='unionid', max_length=255, null=False, blank=False)
+    unionid = models.CharField(verbose_name='unionid', max_length=255, null=True)
 
     nick_name = models.CharField(verbose_name='昵称', max_length=255, null=True)
     avatar = models.CharField(verbose_name='头像', max_length=1000, null=True)
