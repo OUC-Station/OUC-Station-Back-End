6ead34213cb2b8656c24dcc887975f01270beeef
刘阳 <249606097@qq.com>
2021-05-13 13:42:50 +0800

    feat: 扩展 openid unionid 的长度

--- apps/account/models.py
+++ apps/account/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Account(models.Model):
-    openid = models.CharField(verbose_name='openid', max_length=20, null=False, blank=False)
-    unionid = models.CharField(verbose_name='unionid', max_length=20, null=False, blank=False)
+    openid = models.CharField(verbose_name='openid', max_length=255, null=False, blank=False)
+    unionid = models.CharField(verbose_name='unionid', max_length=255, null=False, blank=False)
 
     nick_name = models.CharField(verbose_name='昵称', max_length=255, null=True)
     avatar = models.CharField(verbose_name='头像', max_length=1000, null=True)
